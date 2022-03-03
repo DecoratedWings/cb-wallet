0bdbb98404639f15cecc2a727148e05f329d5ed0
Mona <tigerzmona@yahoo.com>
2022-03-03 17:10:52 -0500

    project setup

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import cblogo from './cb-logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={cblogo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coinbase.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coinbase
         </a>
       </header>
     </div>
